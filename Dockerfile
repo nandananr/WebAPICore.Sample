7b79236912ef15a95a9d61b9f7e3915117f466f8
nandananr <nandana.nalapareddy@gmail.com>
2017-08-24 05:04:27 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM microsoft/dotnet:latest
+COPY src /app
+WORKDIR /app
+RUN ["dotnet", "restore"]
+WORKDIR /app/WebAPICore.Sample
+RUN ["dotnet", "build"]
+EXPOSE 5000/tcp
+ENV ASPNETCORE_URLS http://*:5000
+ENTRYPOINT ["dotnet", "run", "--server.urls", "http://*:5000"]
---
+++ deploy-envs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# set environment variables used in deploy.sh and AWS task-definition.json:
+export IMAGE_NAME=WebAPICore.Sample
+export IMAGE_VERSION=latest
+
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_ECS_CLUSTER_NAME=default
+export AWS_VIRTUAL_HOST=34.231.5.225
+#18.220.138.117
+
+# set any sensitive information in travis-ci encrypted project settings:
+# required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+# optional: SERVICESTACK_LICENSE
